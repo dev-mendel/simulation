fefe7d31db5ddc18648cccd6cf4119f5dfea76db
Dalibor Kramar <dalik.kramar@gmail.com>
2020-10-05 00:14:22 +0200

    Add vizualization of 2nd graph.

--- src/visualization/graphs.py
+++ src/visualization/graphs.py
@@ -5,6 +5,7 @@ import plotly.graph_objs as go
 from plotly.offline import iplot, init_notebook_mode
 
 def getJsonArray(filepath):
+    """deprecated"""
     with open(filepath) as file:
         lines = file.readlines()
         #print(lines[0:4])
@@ -19,18 +20,30 @@ def getJsonArray(filepath):
         return result
         """
 
-def count_objects(object_type, json_array):
+def load_json(filepath):
+    with open(filepath) as file:
+        return json.loads(file.read())
+
+def getDataAtTime(jsonObject):
+    return list(map(lambda item: item["values"], jsonObject["data"]))
+
+def filter_objects(object_type, json_object):
+    json_array = json_object["data"]
+    filtered_arrays = map(lambda item: list(filter(lambda obj: obj["type"] == "ObjectTypes." + object_type, item["values"])), json_array)
+    return list(filtered_arrays)
+
+def count_objects(object_type, json_object):
     """
     Count objects of chosen type from json_array.
     Returns List[int]
     """
-    filtered_arrays = map(lambda item: list(filter(lambda obj: obj["type"] == "ObjectTypes." + object_type, item["values"])), json_array)
+    filtered_arrays = filter_objects(object_type, json_object)
     lengths = map(len, filtered_arrays)
     return list(lengths)
 
-def create_cyanobacteria_and_bacteria_graph(jsonArray, output_filename):
-    ball_count = count_objects("BALL", jsonArray)
-    cyanobacteria_count = count_objects("CYANO_BACTERIA", jsonArray)
+def create_cyanobacteria_and_bacteria_graph(json_object, output_filename):
+    ball_count = count_objects("BALL", json_object)
+    cyanobacteria_count = count_objects("CYANO_BACTERIA", json_object)
 
     trace_ball = go.Scatter(x=list(range(len(ball_count))),
                             y=ball_count,
@@ -51,3 +64,36 @@ def create_cyanobacteria_and_bacteria_graph(jsonArray, output_filename):
     fig = go.Figure(data=data, layout=layout)
     fig.write_html(output_filename)
 
+
+def create_cyanobacteria_and_docked_cyanobacteria_graph(json_object, output_filename):
+    ball_objects = filter_objects("BALL", json_object)
+    docked_cyanobacterias = list(map(lambda balls: sum(map(lambda ball: ball["docked"], balls)), ball_objects))
+    cyanobacteria_count = count_objects("CYANO_BACTERIA", json_object)
+
+    trace_docked = go.Scatter(x=list(range(len(docked_cyanobacterias))),
+                              y=docked_cyanobacterias,
+                              mode="lines",
+                              name="Docked cyanobacteria")
+    trace_cyanobacteria = go.Scatter(x=list(range(len(cyanobacteria_count))),
+                                     y=cyanobacteria_count,
+                                     mode="lines",
+                                     name="All cyanobacteria")
+
+    data = [trace_docked, trace_cyanobacteria]
+
+    layout = {
+        "title": "Dependency of cyanobacteria and docked cyanobacteria on time",
+        "xaxis": {"title":"Iteration number"},
+        "yaxis": {"title":"Object count"},
+        }
+    fig = go.Figure(data=data, layout=layout)
+    fig.write_html(output_filename)
+
+def main():
+    json = load_json(r"..\..\storage\data.json")
+    create_cyanobacteria_and_bacteria_graph(json, "graph1.html")
+    create_cyanobacteria_and_docked_cyanobacteria_graph(json, "graph2.html")
+
+
+if __name__ == "__main__":
+    main()
